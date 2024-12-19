63ba07c651ab7e4789629cca3c90ee04ba02c02b
Gervaise Basa <egygb5@nottingham.ac.uk>
2024-12-19 13:48:39 +0000

    Added more error handlers and refined the code

--- RobotWriter6SkeletonCode/main.c
+++ RobotWriter6SkeletonCode/main.c
@@ -5,8 +5,8 @@
 #include "rs232.h"
 #include "serial.h"
 
-#define bdrate 115200 
-#define FILE1_ARRAY_SIZE 1027              /* 115200 baud */
+#define bdrate 115200 /* 115200 baud */
+#define FILE1_ARRAY_SIZE 1027  // defining file1 array size
 
 void SendCommands (char *buffer );
 //creating structure
@@ -44,33 +44,33 @@ int main()
         return 1;
 
     }
-   
+    // initialising line array for reading file
     char line[256];
-
+    
     while (fgets(line, sizeof(line), file1)) 
     {
-        // Parse each line and store into the struct array
+        // reading each line and storing into the struct array
         if (sscanf(line, "%d %d %d", &File1Array[i].Num1, &File1Array[i].Num2, &File1Array[i].Num3) == 3) {
             i++; // Move to the next struct
         } else {
             fprintf(stderr, "Invalid line format: %s", line);
         }
-        printf("%s",line);
+        //printf("%s",line);
     }
     fclose(file1);
+
     //scale
     int user_input;
-
+    //Creating error handler for first user input
      do {
         printf("\nGive a value between 4 and 10:\n");
         if (scanf("%d", &user_input) != 1) {
-            // Handle invalid input (non-integer)
             printf("Invalid input. Please enter an integer.\n");
-            while (getchar() != '\n'); // Clear input buffer
+            while (getchar() != '\n');
             continue;
         }
 
-        // Check if input is out of range
+        // Checks if input is out of range
         if (user_input < 4 || user_input > 10) {
             printf("Input value not in the range. Try again.\n");
         }
@@ -78,7 +78,7 @@ int main()
 
     // Proceed with valid input
     printf("You entered a valid value: %d\n", user_input);
-    float scaledValue = scale(user_input); // Assuming `scale` is a function
+    float scaledValue = scale(user_input); //using scale function for user input
 
     
     //opening file 2 from user input
@@ -89,21 +89,21 @@ int main()
     scanf("%s" , &textfileInput);
 
     file2 = fopen(textfileInput, "r");
-
+    // file2 error handler
     if (file2 == NULL)
     {
         printf("Error, cannot open file.\n");
         return 1;
 
     }
-    while ((j = fgetc(file2)) != EOF) 
+    while ((j = fgetc(file2)) != EOF) // reads through each letter of file 2
     {
         // Ignores non alphabet characters
-        if (j == '\n' || j == '\r') continue;
+        //if (j == '\n' || j == '\r') continue;
 
         // changes size of array
         char *temp = realloc(TextFileArray, (size + 1) * sizeof(char));
-        if (temp == NULL) 
+        if (temp == NULL) // another error handler
         {
             printf("Error allocating memory");
             free(TextFileArray); // 
@@ -115,7 +115,7 @@ int main()
 
         TextFileArray[size] = j;
         size++;
-        printf("\nvalue read from file is %d\n",TextFileArray[size - 1]);
+        //printf("\nvalue read from file is %d\n",TextFileArray[size - 1]);
     }
     fclose(file2);
 
@@ -152,52 +152,74 @@ int main()
     sprintf (buffer, "S0\n");
     SendCommands(buffer);
 
+
     float xOffset = 0;  // Horizontal offset
     float yOffset = 0;  // Vertical offset
-    float lineSpacing = 10;
-    float charSpacing = 5;  // Distance between lines
+    float lineSpacing = 5; // Distance between lines
     float maxLineWidth = 100;
+    float yvalueLeast = 0;
+    float xvalueMax = 0;
+    char BufferError[100] = {0};
+    char penError[100] = {0};
+
     
-    // These are sample commands to draw out some information - these are the ones you will be generating.
+    // G-code conversion loops
     for (int k = 0; k < size; k++) 
     {
         float asciiValue = (int)TextFileArray[k];
-        if (asciiValue == '\n' || (xOffset + charSpacing * scaledValue) >= maxLineWidth)
+
+        if (asciiValue == '\n' ||  xvalueMax >= maxLineWidth)
         {
             xOffset = 0;  // Reset horizontal offset
-            yOffset -= lineSpacing;  // Increase vertical offset
-            if (TextFileArray[k] == '\n') 
+            yOffset = yvalueLeast-lineSpacing;  // Increase vertical offset
+
+        }
+        // Loop through each entry in the font data array
+        for (int fontline = 0; fontline < FILE1_ARRAY_SIZE; fontline++)
+        {
+            if (File1Array[fontline].Num1 == 999 && File1Array[fontline].Num2 == asciiValue) 
             {
-                continue;
+                int numLines = File1Array[fontline].Num3; 
+                fontline++; // Move to the first line of G-code for this character
+                xvalueMax = 0;
+
+
+                // Output each G-code line for the character
+                for (int line2= 0; line2 < numLines && fontline < FILE1_ARRAY_SIZE; line2++, fontline++)
+                {
+                    float xValue = (File1Array[fontline].Num1 * scaledValue) + xOffset; //setting new x value for g-code with offset and scaled
+                    float yValue = (File1Array[fontline].Num2 * scaledValue) + yOffset; // setting new y value for g-code with offset and scaled
+                    const char* pen = File1Array[fontline].Num3 == 1 ? "S1000" : "S0"; // equals pen variable to up or down depending if value of 3 number is 1 or 0
+                    int gCode = File1Array[fontline].Num3 == 1 ? 1 : 0; //similar code to pen code
+                    sprintf(buffer, " %s\n G%d X%.2f Y%.2f \n", pen, gCode, xValue, yValue);
+
+                     if(buffer == BufferError)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        penError == pen;
+                        BufferError == buffer;                   
+                    }
+                     
+                    SendCommands(buffer);
+
+
+                     if(xvalueMax < xValue){
+                        xvalueMax = xValue;
+                    }
+                    if(yValue<yvalueLeast)
+                    {
+                        yvalueLeast = yValue;
+                    }
+                    
+                } 
+                xOffset = xvalueMax; // increasing offset
+
+                break;
             }
         }
-                // Loop through each entry in the font data array
-                for (int fontline = 0; fontline < FILE1_ARRAY_SIZE; fontline++)
-                {
-                    if (File1Array[fontline].Num1 == 999 && File1Array[fontline].Num2 == asciiValue) 
-                    {
-                        int numLines = File1Array[fontline].Num3; 
-                        fontline++; // Move to the first line of G-code for this character
-                        // Output each G-code line for the character
-                        for (int line2= 0; line2 < numLines && fontline < FILE1_ARRAY_SIZE; line2++, fontline++)
-                        {
-                            float xValue = File1Array[fontline].Num1 * scaledValue + xOffset;
-                            float yValue = File1Array[fontline].Num2 * scaledValue + yOffset;
-                            const char* pen = File1Array[fontline].Num3 == 1 ? "S1000" : "S0";
-                            int gCode = File1Array[fontline].Num3 == 1 ? 1 : 0;
-                            SendCommands(pen);
-
-                            sprintf(buffer, "G%d X%.2f Y%.2f \n",gCode, xValue, yValue);
-                            //printf(pen);
-                            //SendCommands(pen);
-                            //printf("%s",buffer);
-                            SendCommands(buffer);
-                        }
-                        xOffset += charSpacing  ;
-                        break;
-                    }
-                //break;
-                }
     }
 
     // Before we exit the program we need to close the COM port
